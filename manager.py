b09708397c455dc23986544c3b21c34e2fd8438b
Jennifer Yu <z5480946@ad.unsw.edu.au>
2024-11-10 16:41:33 +1100

    Add: added password scan functionality for stored passwords, listing vulnerabilities and warnings

--- manager.py
+++ manager.py
@@ -8,8 +8,12 @@ from argon2 import PasswordHasher
 ph = PasswordHasher(time_cost=1, memory_cost=47104, parallelism=1, hash_len=32, salt_len=16, type=argon2.Type.ID)
 
 
-### Character set used for salt and pepper-ing
+### Character set used for salt and pepper-ing ###
 character_set = string.ascii_letters + string.digits + string.punctuation
+
+### File names as a constant ###
+PASSWORD_FILE = 'passwords.json'
+USER_FILE = 'user_data.json'
 
 ### Encryption and Decryption ###
 
@@ -52,11 +56,9 @@ def verify_with_pepper(entered_password, stored_password_hash):
 
 
 # check before salt and pepper is added
-def password_vulnerabilities_check(username, password):
+def password_vulnerabilities_check(password):
     vulnerabilities = []
 
-    if username == password:
-        vulnerabilities.append("- Password is the same as username")
     if len(password) < 8:
         vulnerabilities.append("- Password length is less than 8")
     with open("PwnedPasswordsTop100k.txt") as f:
@@ -64,14 +66,61 @@ def password_vulnerabilities_check(username, password):
         if password in common_passwords:
             index = common_passwords.index(password)
             # check if within top 200 used passwords
-            if index < 200:
-                vulnerabilities.append("- Password is in top 200 most common passwords")
+            if index < 1000:
+                vulnerabilities.append("- Password is in top 1000 most common passwords")
     
     f.close()
 
     return vulnerabilities
 
+def password_warnings_check(password):
+    warnings = []
+    if not any(c.islower() for c in password):
+        warnings.append("- Password does not contain at least one lowercase character")
+    if not any(c.isupper() for c in password):
+        warnings.append("- Password does not contain at least one uppercase character")
+    if not any(c.isdigit() for c in password):
+        warnings.append("- Password does not contain at least one digit character")
+    if not any(c in string.punctuation for c in password):
+        warnings.append("- Password does not contain at least one punctuation character")
+    
+    with open("PwnedPasswordsTop100k.txt") as f:
+        common_passwords = f.read().splitlines()
+        if password in common_passwords:
+            index = common_passwords.index(password)
+            # check if within top 200 used passwords
+            if index >= 1000:
+                warnings.append("- Password is a commonly used password")
+    
+    f.close()
+    return warnings
 
+
+def scan_passwords():
+    try:
+       with open(PASSWORD_FILE, 'r') as data:
+            print("Scanning website passwords...\n")
+            view = json.load(data)
+            passwords = []
+            for x in view:
+                website = x['website']
+                stored_password = get_password(website)
+                passwords.append(stored_password)
+                print(f'[+] Website: {website}:')
+                vulnerabilities = password_vulnerabilities_check(stored_password)
+                warnings = password_warnings_check(stored_password)
+                print(f'Found {len(vulnerabilities)} vulnerabilities:')
+                for v in vulnerabilities:
+                    print(v)
+                print(f'\nFound {len(warnings)} warnings:')
+                for w in warnings:
+                    print(w)
+                print('\n')
+            if len(set(passwords)) != len(passwords):
+                print("----Attention: two or more passwords are the same----")
+            print('\n')
+    except FileNotFoundError:
+       print("\n[-] You have not saved any passwords!\n")
 
 ### Features on Password Manager ###
 # Registers the admin of this system and the master password
@@ -81,7 +130,9 @@ def register(username, master_password):
     # Generate a pepper to append
     pepper = generate_pepper()
 
-    issues = password_vulnerabilities_check(username, master_password)
+    issues = password_vulnerabilities_check(master_password)
+    if username == master_password:
+        issues.append("- Password is the same as username")
     if len(issues) > 0:
         print('\nWARNING --- your inputted password has the following vulnerabilities\n')
         for line in issues:
@@ -91,7 +142,7 @@ def register(username, master_password):
     # uses argon2 to hash the master password
     hashed_master_password = ph.hash(master_password + salt + pepper)
     user_data = {'username': username, 'master_password': hashed_master_password, 'salt' : salt}
-    file_name = 'user_data.json'
+    file_name = USER_FILE
     if os.path.exists(file_name) and os.path.getsize(file_name) == 0:
         with open(file_name, 'w') as file:
             json.dump(user_data, file)
@@ -104,7 +155,7 @@ def register(username, master_password):
 
 # Takes in a username and password and attempts to log in
 def login(username, entered_password):
-    with open('user_data.json', 'r') as file:
+    with open(USER_FILE, 'r') as file:
         user_data = json.load(file)
     stored_password_hash = user_data.get('master_password')
     if verify_with_pepper(entered_password + user_data.get('salt'), stored_password_hash) and username == user_data.get('username'):
@@ -117,7 +168,7 @@ def login(username, entered_password):
 # Displays al the saved websites
 def view_websites():
    try:
-       with open('passwords.json', 'r') as data:
+       with open(PASSWORD_FILE, 'r') as data:
            view = json.load(data)
            print("\nWebsites you saved...\n")
            for x in view:
@@ -143,13 +194,13 @@ cipher = initialize_cipher(key)
 # Function to add and save passwords tied to a particular website
 def add_password(website, password):
    # Check if passwords.json exists
-   if not os.path.exists('passwords.json'):
+   if not os.path.exists(PASSWORD_FILE):
        # If passwords.json doesn't exist, initialize it with an empty list
        data = []
    else:
        # Load existing data from passwords.json
        try:
-           with open('passwords.json', 'r') as file:
+           with open(PASSWORD_FILE, 'r') as file:
                data = json.load(file)
        except json.JSONDecodeError:
            # Handle the case where passwords.json is empty or invalid JSON.
@@ -160,18 +211,18 @@ def add_password(website, password):
    password_entry = {'website': website, 'password': encrypted_password}
    data.append(password_entry)
    # Save the updated list back to passwords.json
-   with open('passwords.json', 'w') as file:
+   with open(PASSWORD_FILE, 'w') as file:
        json.dump(data, file, indent=4)
 
 
 # Function to retrieve a saved password.
 def get_password(website):
    # Check if passwords.json exists
-   if not os.path.exists('passwords.json'):
+   if not os.path.exists(PASSWORD_FILE):
        return None
    # Load existing data from passwords.json
    try:
-       with open('passwords.json', 'r') as file:
+       with open(PASSWORD_FILE, 'r') as file:
            data = json.load(file)
    except json.JSONDecodeError:
        data = []
@@ -191,7 +242,7 @@ while True:
    print("3. Quit")
    choice = input("Enter your choice: ")
    if choice == '1':  # If a user wants to register
-       file = 'user_data.json'
+       file = USER_FILE
        if os.path.exists(file) and os.path.getsize(file) != 0:
            print("\n[-] Master user already exists!!")
            sys.exit()
@@ -204,7 +255,7 @@ while True:
             else:
                 register(username, master_password)
    elif choice == '2':  # If a User wants to log in
-       file = 'user_data.json'
+       file = USER_FILE
        if os.path.exists(file):
            username = input("Enter your username: ")
            master_password = getpass.getpass("Enter your master password: ")
@@ -214,31 +265,34 @@ while True:
            sys.exit()
        # Various options after a successful Login.
        while True:
-           print("1. Add Password")
-           print("2. Get Password")
-           print("3. View Saved websites")
-           print("4. Quit")
-           password_choice = input("Enter your choice: ")
-           if password_choice == '1':  # If a user wants to add a password
-               website = input("Enter website: ")
-               password = getpass.getpass("Enter password: ")
-               # Encrypt and add the password
-               add_password(website, password)
-               print("\n[+] Password added!\n")
-           elif password_choice == '2':  # If a User wants to retrieve a password
-               website = input("Enter website: ")
-               decrypted_password = get_password(website)
-               if website and decrypted_password:
-                   # Copy password to clipboard for convenience
-                   pyperclip.copy(decrypted_password)
-                   print(f"\n[+] Password for {website}: {decrypted_password}\n[+] Password copied to clipboard.\n")
-               else:
-                   print("\n[-] Password not found! Did you save the password?"
-                         "\n[-] Use option 3 to see the websites you saved.\n")
-           elif password_choice == '3':  # If a user wants to view saved websites
-               view_websites()
-           elif password_choice == '4':  # If a user wants to quit the password manager
-               break
+            print("1. Add Password")
+            print("2. Get Password")
+            print("3. View Saved websites")
+            print("4. Scan all passwords")
+            print("5. Quit")
+            password_choice = input("Enter your choice: ")
+            if password_choice == '1':  # If a user wants to add a password
+                website = input("Enter website: ")
+                password = getpass.getpass("Enter password: ")
+                # Encrypt and add the password
+                add_password(website, password)
+                print("\n[+] Password added!\n")
+            elif password_choice == '2':  # If a User wants to retrieve a password
+                website = input("Enter website: ")
+                decrypted_password = get_password(website)
+                if website and decrypted_password:
+                    # Copy password to clipboard for convenience
+                    pyperclip.copy(decrypted_password)
+                    print(f"\n[+] Password for {website}: {decrypted_password}\n[+] Password copied to clipboard.\n")
+                else:
+                    print("\n[-] Password not found! Did you save the password?"
+                            "\n[-] Use option 3 to see the websites you saved.\n")
+            elif password_choice == '3':  # If a user wants to view saved websites
+                view_websites()
+            elif password_choice == '4': # If a user wants to scan current passwords for security
+                scan_passwords()
+            elif password_choice == '5':  # If a user wants to quit the password manager
+                break
    elif choice == '3':  # If a user wants to quit the program
        break
    
